f13b291f4f625050cc589c9e6e9dc3b5a02e646b
Alona Kaptur <alonamsm93@gmail.com>
2019-02-17 23:38:46 +0200

    Added HW #1 solution

---
+++ HW_01/src/com/gmail/alonamsm93/Main.java
@@ -0,0 +1,15 @@
+package com.gmail.alonamsm93;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(" * * * *     * * * *        *           *        *  ");
+        System.out.println("                                                    ");
+        System.out.println(" *           *     *      *   *         * *    * *  ");
+        System.out.println("                                                    ");
+        System.out.println(" * *         * * * *     *     *        *    *   *  ");
+        System.out.println("                                                    ");
+        System.out.println(" *           *          * * * * *       *        *  ");
+        System.out.println("                                                    ");
+        System.out.println(" * * * *     *         *         *      *        *  ");
+    }
+}
